e804d2a5a7376707c900858b1ba9b618f4752a12
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-06-19 13:39:20 -0400

    lab4 - basic graph

--- Lab4/js/main.js
+++ Lab4/js/main.js
@@ -2,13 +2,89 @@
 // SVG Size
 var width = 700,
 	height = 500;
+var padding = 30;
 
 
 // Load CSV file
 d3.csv("data/wealth-health-2014.csv", function(data){
 
 	// Analyze the dataset in the web console
-	console.log(data);
 	console.log("Countries: " + data.length)
+	data  = typechange(data); 
+	console.log(data);
+	renderChart(data);
 
 });
+
+function typechange(data){ 
+	// Type converting Income , population and LifeExpectancy 
+	data.forEach(element => {
+		element["Income"] = parseFloat(element["Income"]);
+		element.LifeExpectancy = parseFloat(element.LifeExpectancy);
+		element.Population = parseInt(element.Population);
+		
+	});
+	return data;
+}
+
+function renderChart(data){
+	var svg = d3.select("#chart-area")
+			.append("svg")
+			.attr("width",width)
+			.attr("height",height);
+	
+	var incomeScale = d3.scaleLinear()
+			.domain([d3.min(data,function(d){  return d.Income   }) - 100 ,d3.max(data,function(d){return d.Income}) - 100 ])
+			.range([padding , width - padding]);
+	console.log(incomeScale(5000));
+
+	var LifeExpectancyScale = d3.scaleLinear()
+			.domain([d3.min(data,function(d){ return d.LifeExpectancy }) - 5 ,d3.max(data,function(d){ return d.LifeExpectancy }) ])
+			.range([height - padding ,padding ]);
+	console.log(LifeExpectancyScale(68));
+
+	var radiusScale = d3.scaleLinear()
+			.domain([d3.min(data,function(d){ return d.Population }) - 5 ,d3.max(data,function(d){ return d.Population }) ])
+			.range([4,30]);
+	
+	svg.selectAll("circle")
+			.data(data)
+			.enter()
+			.append("circle")
+			.attr("fill","olive")
+			.attr("cx",function(d){
+				return incomeScale(d.Income);
+			})
+			.attr("cy",function(d){
+				return LifeExpectancyScale(d.LifeExpectancy);
+			})
+			.attr("r",function(d){
+				return radiusScale(d.Population);
+			})
+	var xAxis = d3.axisBottom().scale(incomeScale)
+				.ticks(10);
+
+	var yAxis = d3.axisLeft().scale(LifeExpectancyScale)
+				.ticks(8)
+				.tickValues([45,50,55,60,65,70,75,80]);
+
+	svg.append("g")
+			.attr("class","axis x-axis")
+			.attr("transform","translate(0,"+(height-padding)+")")
+			.call(xAxis);
+	
+	svg.append("g")
+			.attr("class","axis y-axis")
+			.attr("transform","translate(" + (padding) +",0)")
+			.call(yAxis);
+	
+	svg.append("g").append("text").text("Income")
+			.attr("class","x-axis-name")
+			.attr("y",height)
+			.attr("x",width/2)	
+	svg.append("g").append("text").text("Life Expectancy")
+			.attr("class","y-axis-name")
+			.attr("y",height/2)
+			.attr("x",0);
+	
+}
