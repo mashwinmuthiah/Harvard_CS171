0e30c5fbc0cccb2f7712d8ec6c939c4db1bf6ae8
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-07-09 11:20:02 -0400

    final

--- Mastering_Vis/js/main.js
+++ Mastering_Vis/js/main.js
@@ -1,18 +1,33 @@
-var svg = d3.select("#chart-area").append("svg")
+d3.json("/data/ages.json").then(function(data){
+    data.forEach(element => {
+        element.age = +element.age
+    });
+    console.log(data);
+    renderChart(data);
+});
+
+function renderChart(data){
+    var svg = d3.select("#chart-area").append("svg")
     .attr("width",400)
     .attr("height",400);
 
-var data = [25,30,35,40,45,50];
-
-var circle = svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-        .attr("cx",(d,i) =>{
+    var circle = svg.selectAll("circle")
+        .data(data)
+        .enter()
+            .append("circle")
+            .attr("cx",(d,i) =>{
             return i*50+25;
-        })
-        .attr("cy",200)
-        .attr("r",(d,i)=>{
-            return d;
-        })
-        .attr("fill","blue")
+             })
+            .attr("cy",200)
+            .attr("r",(d,i)=>{
+                 return d.age *2;
+                })
+            .attr("fill",(d,i)=>{
+                if(d.name == "Tony"){
+                    return "blue";
+                }
+                else{
+                    return "red";
+                }
+            });
+} 
