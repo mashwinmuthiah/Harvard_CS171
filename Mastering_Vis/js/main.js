3ef866bdd0e9f18c3ce261627815036a5b662158
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-07-09 10:51:11 -0400

    js/main:

--- Mastering_Vis/js/main.js
+++ Mastering_Vis/js/main.js
@@ -2,8 +2,17 @@ var svg = d3.select("#chart-area").append("svg")
     .attr("width",400)
     .attr("height",400);
 
-var circle = svg.append("circle")
-    .attr("cx",200)
-    .attr("cy",200)
-    .attr("r",100)
-    .attr("fill","blue")
+var data = [25,30,35,40,45,50];
+
+var circle = svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+        .attr("cx",(d,i) =>{
+            return i*50+25;
+        })
+        .attr("cy",200)
+        .attr("r",(d,i)=>{
+            return d;
+        })
+        .attr("fill","blue")
