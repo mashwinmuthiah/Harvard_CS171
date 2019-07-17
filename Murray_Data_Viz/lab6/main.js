7830e4ab1732f9e1c108872a60e988aa7cdd29ea
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-07-17 14:35:33 -0400

    lab6

---
+++ Murray_Data_Viz/lab6/main.js
@@ -0,0 +1,16 @@
+var x = "outside";
+var f1 = function() {
+ var x = "inside f1";
+};
+f1();
+console.log(x);
+// ! outside
+
+
+x = "hi";
+var f2 = function() {
+x = "inside f2";
+};
+f2();
+console.log(x);
+// ! inside f2
